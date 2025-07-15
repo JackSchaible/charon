e44d1ef4ec0883fd86895da408c7fdf74e139521
Cartoffel <jack.schaible@hotmail.com>
2025-07-15 16:51:43 -0600

    Add pings

--- server/Functions/ApiFunction.cs
+++ server/Functions/ApiFunction.cs
@@ -1,6 +1,35 @@
 namespace API.Functions;
 
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
 public class ApiFunction
 {
-    
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        context.Logger.LogLine($"Received request: {request.HttpMethod} {request.Path}");
+
+        if (request.Path == "/api/ping")
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Body = "Hello from API Function!",
+                Headers = new Dictionary<string, string>
+                {
+                    { "Content-Type", "text/plain" }
+                }
+            };
+        
+        // TODO: Handle other API routes here
+
+        return await Task.FromResult(new APIGatewayProxyResponse
+        {
+            StatusCode = 404,
+            Body = "Not Found",
+            Headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "text/plain" }
+            }
+        });
+    }
 }
